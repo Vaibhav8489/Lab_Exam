73f64db35b41926cbd9ad7bbed04d3bf93cec1ca
Vaibhav8489 <122714546+Vaibhav8489@users.noreply.github.com>
2023-01-15 12:37:32 +0530

    Vaibhav Bhapkar PRN:0013
     OS Lab Exam

---
+++ NumberDivisibility.java
@@ -0,0 +1,35 @@
+package num;
+
+import java.util.Scanner;
+
+class NumberDivisibility
+{
+
+	static String isDivisible(long n)
+	{
+		long temp = n;
+	
+		int sum = 0;
+		while (n != 0)
+		{
+			int k = (int) n % 10;
+			sum += k;
+			n /= 10;
+		}
+	
+		if (temp % sum == 0)
+			return "YES";
+	
+		return "NO";
+	}
+	
+	public static void main(String []args)
+	{
+		System.out.println("Enter a number: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(isDivisible(n));
+		sc.close();
+	}
+}
+
